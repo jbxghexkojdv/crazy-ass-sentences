658e11e39d8a8f865804cb8d368e4f98ff248d27
jbxghexkojdv <107658001+jbxghexkojdv@users.noreply.github.com>
2022-06-25 17:42:43 -0500

    hopefully i can test now

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-import sentences from 'cas.json' assert {type: 'json'};
+let a = new XMLHttpRequest();
+a.open("GET", "./cas.json", true);
+let sentences;
+a.onload = () => 
+{
+  sentences = JSON.parse(a.responseText);
+};
+a.send();
 document.getElementById("sentence").innerHTML = JSON.stringify(sentences);
