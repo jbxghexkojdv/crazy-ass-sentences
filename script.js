1b94daa9ef2ab3ab96b0328590516568c099c386
jbxghexkojdv <107658001+jbxghexkojdv@users.noreply.github.com>
2022-06-27 12:20:07 -0500

    fine
    fine the json is for the python only

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-let a = new XMLHttpRequest();
-a.open("GET", "./cas.json", true);
-let sentences;
-a.onload = () => 
-{
-  sentences = JSON.parse(a.responseText);
+const sentences = {
+  "english": 
+  {
+    "beginnings": ["I am less pregnant if I drink", "I threw a chair at", "I am listening to"],
+    "endings": ["alcohol", "Donald Trump", "Lena Raine's Labyrithine"],
+    "punc": [[".", "!", "?"], ["", "", ""]]
+  },
+  "spanish":
+  {
+    "beginnings": ["Estoy menos embarazada si bebo", "Le lancé una silla a", "Estoy escuchando"],
+    "endings": ["alcohol", "Donald Trump", "Labyrinthine de Lena Raine"],
+    "punc": [[".", "!", "?"], ["", "¡", "¿"]]
+  }
 };
-a.send();
-document.getElementById("sentence").innerHTML = a.status;
+
+document.getElementById("sentence").innerHTML = JSON.stringify(sentences);
