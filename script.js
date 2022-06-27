8b649d62f627584e0e438353c2602ff9aab3759b
jbxghexkojdv <107658001+jbxghexkojdv@users.noreply.github.com>
2022-06-27 13:04:04 -0500

    add forward, back and generation

--- script.js
+++ script.js
@@ -20,4 +20,45 @@ function randelem(arr)
 
 let generatedSentences;
 
+let sentenceNum = 0;
+
+function create(lang)
+{
+  const puncnum = Math.floor(Math.random()*3);
+  let retval = "";
+  retval += sentences[lang].punc[1][puncnum];
+  retval += randelem(sentences[lang].beginnings) + " ";
+  retval += randelem(sentences[lang].endings);
+  retval += sentences[lang].punc[0][puncnum];
+  return retval;
+}
+
+function generate()
+{
+  const amount = document.getElementById("number").value;
+  const lang = document.getElementById("lang").value;
+  generatedSentences = [];
+  for (let i = 0; i < amount; i++)
+  {
+    switch(lang)
+    {
+      case "spanish":
+      case "español":
+        generatedSentences.push(create("spanish"));
+        break;
+      default:
+        generatedSentences.push(create("english"));
+  }
+  document.getElementById("sentence").innerHTML = generatedSentences[0] + "<br /> Sentence 1 of " + amount;
+  sentenceNum = 0;
+}
+
+function forward()
+{
+  let display = document.getElementById("sentence");
+  sentenceNum++;
+  sentenceNum %= generatedSentences.length;
+  display.innerHTML = generatedSentences[sentenceNum] + "<br />Sentence " + `${sentenceNum + 1} of ${generatedSentences.length}`
+}
+
 document.getElementById("sentence").innerHTML = "{ }";
